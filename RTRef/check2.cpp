ece283ea656ce8b6cfaa295b8f66dbc409eb1664
mattquinn56 <mtq6@cornell.edu>
2022-02-23 16:08:20 -0500

    neaten

--- RTRef/check2.cpp
+++ RTRef/check2.cpp
@@ -26,7 +26,13 @@
 #  include <windows.h>
 #endif
 
+#if defined(RTC_NAMESPACE_USE)
+RTC_NAMESPACE_USE
+#endif
+
+
 /**************************************** STRUCTURES ****************************************/
+
 
 struct Color {
     unsigned char r;
@@ -112,65 +118,20 @@ public:
     }
 };
 
+
 /**************************************** GIVEN FUNCTIONS ****************************************/
 
-/*
- * A minimal tutorial. 
- *
- * It demonstrates how to intersect a ray with a single triangle. It is
- * meant to get you started as quickly as possible, and does not output
- * an image. 
- *
- * For more complex examples, see the other tutorials.
- *
- * Compile this file using
- *   
- *   gcc -std=c99 \
- *       -I<PATH>/<TO>/<EMBREE>/include \
- *       -o minimal \
- *       minimal.c \
- *       -L<PATH>/<TO>/<EMBREE>/lib \
- *       -lembree3 
- *
- * You should be able to compile this using a C or C++ compiler.
- */
 
-/* 
- * This is only required to make the tutorial compile even when
- * a custom namespace is set.
- */
-#if defined(RTC_NAMESPACE_USE)
-RTC_NAMESPACE_USE
-#endif
-
-/*
- * We will register this error handler with the device in initializeDevice(),
- * so that we are automatically informed on errors.
- * This is extremely helpful for finding bugs in your code, prevents you
- * from having to add explicit error checking to each Embree API call.
- */
 void errorFunction(void* userPtr, enum RTCError error, const char* str) {
   printf("error %d: %s\n", error, str);
 }
 
-/*
- * Embree has a notion of devices, which are entities that can run 
- * raytracing kernels.
- * We initialize our device here, and then register the error handler so that 
- * we don't miss any errors.
- *
- * rtcNewDevice() takes a configuration string as an argument. See the API docs
- * for more information.
- *
- * Note that RTCDevice is reference-counted.
- */
 RTCDevice initializeDevice() {
   RTCDevice device = rtcNewDevice(NULL);
   if (!device) printf("error %d: cannot create device\n", rtcGetDeviceError(NULL));
   rtcSetDeviceErrorFunction(device, errorFunction, NULL);
   return device;
 }
-
 
 void waitForKeyPressedUnderWindows() {
 #if defined(_WIN32)
@@ -189,7 +150,9 @@ void waitForKeyPressedUnderWindows() {
 #endif
 }
 
+
 /**************************************** SCENE AND RAY ****************************************/
+
 
 /*
  * Create a scene, which is a collection of geometry objects. Scenes are 
@@ -345,7 +308,9 @@ Color castRay(RTCScene scene,
   return Color();
 }
 
+
 /**************************************** MAIN ****************************************/
+
 
 int main() {
     Assimp::Importer importer;
