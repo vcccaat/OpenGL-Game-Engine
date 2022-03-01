b0a63789e242d373933dec2e3549bbf294124711
mattquinn56 <mtq6@cornell.edu>
2022-03-01 11:28:56 -0500

    better pathing

--- RTRef/check2.cpp
+++ RTRef/check2.cpp
@@ -322,8 +322,7 @@ struct Environment {
 };
 
 void updateImgData(std::vector<glm::vec3>& img_data, int width, int height) {
-    // Paths: C:/Users/Ponol/Documents/GitHub/Starter22/resources/scenes/bunnyscene.glb
-    //        ../resources/scenes/bunnyscene.glb
+    //Environment env("../resources/scenes/bunnyscene.glb", width, height);
     Environment env("C:/Users/Ponol/Documents/GitHub/Starter22/resources/scenes/bunnyscene.glb", width, height);
     env.rayTrace(img_data);
 }
