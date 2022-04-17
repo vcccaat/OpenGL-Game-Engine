00dff750ce8bdce7f07ea9a706696fe5bc0f099f
mattquinn56 <mtq6@cornell.edu>
2022-04-16 23:57:59 -0400

    fixed?

--- Pipeline/main.cpp
+++ Pipeline/main.cpp
@@ -5,11 +5,11 @@ int main(int argc, char const* argv[]) {
     nanogui::init();
 
     // PATHEDIT
-    const std::string path = "../resources/scenes/tree.glb";
+    //const std::string path = "../resources/scenes/tree.glb";
     // const std::string path = "../resources/scenes/bunnyscene2.glb";
     //const std::string path = "../resources/scenes/smoothbunny.glb";
     
-    // const std::string path = "C:/Users/Ponol/Documents/GitHub/Starter22/resources/scenes/tree.glb";
+    const std::string path = "C:/Users/Ponol/Documents/GitHub/Starter22/resources/scenes/bunnyscene2.glb";
     //const std::string path = "C:/Users/Ponol/Documents/GitHub/Starter22/resources/scenes/smoothbunny.glb";
 	
     float aspect = getAspect(path);
